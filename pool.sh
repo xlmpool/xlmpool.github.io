37ac2d33f986ccd429d3bbdae4f26aebabafc2ee
xlmpool <748933799@qq.com>
2017-06-13 12:18:34 +0800

    Add files via upload

---
+++ pool.sh
@@ -0,0 +1,5 @@
+wget url https://fed.network/inflation/GA3FUYFOPWZ25YXTCA73RK2UGONHCO27OHQRSGV3VCE67UEPEFEDCOPA
+
+#Execute every five minutes to keep the snapshot of the vote.
+#Then check the balance of the inflation address to count the votes. 
+#Adjust the accuracy by the length of time.
